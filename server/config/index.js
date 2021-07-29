85aa5056ad85fac8ac0d327aac4f33cc68a61f37
Nicholas Zanotto <nicholas.zanotto@hotmail.com>
2021-07-29 12:24:37 +0200

    deploy

--- server/config/index.js
+++ server/config/index.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 module.exports = (app) => {
   app.use(logger("dev"));
-
+app.use(express.static(path.join(__dirname, "..", "public")));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
