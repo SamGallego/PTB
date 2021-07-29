982f957c0b9e6211f6324701d9c3f6c03df3cfa4
Nicholas Zanotto <nicholas.zanotto@hotmail.com>
2021-07-29 12:06:51 +0200

    deploy

--- client/src/components/pages/Index/IndexPage.js
+++ client/src/components/pages/Index/IndexPage.js
@@ -1,15 +1,15 @@
 import { Container } from 'react-bootstrap'
 import './IndexPage.css'
-import background from './background.jpg'
+
 
 const Index = () => {
 
 
     return (
-        <Container>
-            <h1>Welcome to PTB!</h1>
+        <Container className='home'>
+            <h1 className='welcome'>Welcome to PTB!</h1>
             <h1 class='football'> Football is more than a sport, it's a life style</h1>
-            <p>Let's play!</p>
+            <p className='play'>Let's play!</p>
            
 
         </Container>
